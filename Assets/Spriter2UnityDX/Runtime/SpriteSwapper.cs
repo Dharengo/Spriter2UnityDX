e1ae3b73e1134de1a18972d52a3d0edce9840276
Shawn <shawn@gskinner.com>
2015-05-12 15:02:23 -0600

    Add additional API's to SpriteSwapper to allow you to Swap and Unswap by Name

--- Assets/Spriter2UnityDX/Runtime/SpriteSwapper.cs
+++ Assets/Spriter2UnityDX/Runtime/SpriteSwapper.cs
@@ -16,7 +16,8 @@ namespace Spriter2UnityDX {
 	public class SpriteSwapper : MonoBehaviour {
 		public float DisplayedSprite = 0f; //Input from the AnimationClip
 		public Sprite[] Sprites; //If you want to swap textures at runtime, change the sprites in this array
-
+		Sprite[] origSprites;
+		
 		private SpriteRenderer srenderer;
 		private Animator animator;
 		private int lastDisplayed;
@@ -29,21 +30,53 @@ namespace Spriter2UnityDX {
 
 		private void Start () {
 			srenderer.sprite = Sprites [lastDisplayed];
+			//Make a copy of the original array, so we can easily unswap any of our sprites
+            if (Sprites != null) {
+                origSprites = (Sprite[])Sprites.Clone();
+            }
 		}
 
 		private void Update () {
 			//Only change the sprite when the DisplayedSprite property has actually been changed
 			//It will ignore changes that happen during transitions because it might get messy otherwise
-			if ((int)DisplayedSprite != lastDisplayed && !IsTransitioning () ) {
-				lastDisplayed = (int)DisplayedSprite;
-				srenderer.sprite = Sprites [lastDisplayed];
-			}
+			if (((int)DisplayedSprite != lastDisplayed && !IsTransitioning())) {
+                UpdateSprite();
+            }
 		}
+
+        private void UpdateSprite(bool force = false) {
+            lastDisplayed = (int)DisplayedSprite;
+            srenderer.sprite = Sprites[lastDisplayed];
+        }
 
 		private bool IsTransitioning () {
 			for (var i = 0; i < animator.layerCount; i++)
 				if (animator.IsInTransition(i)) return true;
 			return false;
 		}
+
+		//Replace one sprite with another by name
+        public void Swap(string targetName, string swapName) {
+            for (int i = 0, l = origSprites.Length; i < l; i++) {
+                //Find slot corresponding to the target name
+                if (origSprites[i].name == targetName) {
+                    //Find the a replacement sprite and swap it in the correct slot
+                    foreach (var s in origSprites) {
+                        if (s.name == swapName) { Sprites[i] = s; }
+                    }
+                }
+            }
+            UpdateSprite(true);
+        }
+
+        //Revert a sprite to it's original value
+        public void Unswap(string target) {
+            for (int i = 0, l = origSprites.Length; i < l; i++) {
+                if (origSprites[i].name == target) {
+                    Sprites[i] = origSprites[i];
+                }
+            }
+            UpdateSprite(true);
+        }
 	}
 }
