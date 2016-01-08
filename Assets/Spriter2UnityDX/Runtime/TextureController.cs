9011b10720d3ebeec180108237541d477c246dad
plagas36 <alex.rousseau36@gmail.com>
2016-01-08 13:25:14 +0100

    Update TextureController.cs
    Add folder variable

--- Assets/Spriter2UnityDX/Runtime/TextureController.cs
+++ Assets/Spriter2UnityDX/Runtime/TextureController.cs
@@ -1,4 +1,4 @@
-﻿//This project is open source. Anyone can use any part of this code however they wish
+//This project is open source. Anyone can use any part of this code however they wish
 //Feel free to use this code in your own projects, or expand on this code
 //If you have any improvements to the code itself, please visit
 //https://github.com/Dharengo/Spriter2UnityDX and share your suggestions by creating a fork
@@ -7,42 +7,50 @@
 using UnityEngine;
 using System.Collections;
 
-namespace Spriter2UnityDX {
+namespace Spriter2UnityDX
+{
 	//This component is automatically added to sprite parts that have multiple possible
 	//textures, such as facial expressions. This component will override any changes
 	//you make to the SpriteRenderer's textures, so if you want to change textures
 	//at runtime, please make these changes to this component, rather than SpriteRenderer
 	[RequireComponent (typeof(SpriteRenderer)), DisallowMultipleComponent, ExecuteInEditMode, AddComponentMenu("")]
-	public class TextureController : MonoBehaviour {
+	public class TextureController : MonoBehaviour
+	{
 		public float DisplayedSprite = 0f; //Input from the AnimationClip
 		public Sprite[] Sprites; //If you want to swap textures at runtime, change the sprites in this array
+		public int folder = -1;
 
 		private SpriteRenderer srenderer;
 		private Animator animator;
 		private int lastDisplayed;
 		
-		private void Awake () {
+		private void Awake ()
+		{
 			srenderer = GetComponent<SpriteRenderer> ();
 			lastDisplayed = (int)DisplayedSprite;
 			animator = GetComponentInParent<Animator> ();
 		}
 
-		private void Start () {
+		private void Start ()
+		{
 			srenderer.sprite = Sprites [lastDisplayed];
 		}
 
-		private void Update () {
+		private void Update ()
+		{
 			//Only change the sprite when the DisplayedSprite property has actually been changed
 			//It will ignore changes that happen during transitions because it might get messy otherwise
-			if ((int)DisplayedSprite != lastDisplayed && !IsTransitioning () ) {
+			if ((int)DisplayedSprite != lastDisplayed && !IsTransitioning ()) {
 				lastDisplayed = (int)DisplayedSprite;
 				srenderer.sprite = Sprites [lastDisplayed];
 			}
 		}
 
-		private bool IsTransitioning () {
+		private bool IsTransitioning ()
+		{
 			for (var i = 0; i < animator.layerCount; i++)
-				if (animator.IsInTransition(i)) return true;
+				if (animator.IsInTransition (i))
+					return true;
 			return false;
 		}
 	}
